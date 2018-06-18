9b0904707aff303a43bcec883af4c0307fb04a82
jackdoesdata <jackdoesdata@gmail.com>
2018-06-17 20:47:19 -0700

    updated gsheet api with gsheet function

--- gSheetAPI.py
+++ gSheetAPI.py
@@ -1,3 +1,4 @@
+# Import packages
 import pandas as pd
 import numpy as np
 import datetime as dt
@@ -7,24 +8,36 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
+# Create vars for gsheet API authentication
 scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('pipelineTutorial-8fff96610970.json', scope)
 client = gspread.authorize(creds)
-sheet = client.open('pipelineSampleTable').sheet1
-data = pd.DataFrame(sheet.get_all_values())
-header = data.iloc[0]
-data = data[1:]
-data.columns = header
 
+
+# Function for hooking into gsheets
+def gSheet(sheetName):
+    sheet = client.open(sheetName).sheet1
+    data = pd.DataFrame(sheet.get_all_values())
+    header = data.iloc[0]
+    data = data[1:]
+    data.columns = header
+    return data
+data = gSheet('pipelineSampleTable')
+
+
+# Change dtypes on date columns
 dates = ['DOB', 'signUpDate']
-
 for i in dates:
     data[i] = pd.to_datetime(data[i], infer_datetime_format=True)
     data[i] = data[i].dt.date
 
-data['firstName'] = data['Name'].map(lambda x:x.split(' ', 1)[0])
-data['lastName'] = data['Name'].map(lambda x:x.split(' ', 1)[-1])
 
+# Parse name entry for first and last columns
+data['firstName'] = data['Name'].apply(lambda x:x.split(' ')[0])
+data['lastName'] = data['Name'].apply(lambda x:x.split(' ')[-1])
+
+
+# Create connection to database and push dataframe. Will replace old table.
 engine = create_engine('mysql://jack:jack1620@tutorials.clpvlhaqk6id.us-east-1.rds.amazonaws.com:3306/tutorialDB')
 data.to_sql(name='pipelineTable', con=engine, if_exists='replace', index=False, chunksize=1000)
